d8b646391260f86755d0c0a5396044b556ab138a
Bikbokken <benjaminburek05@gmail.com>
2023-01-30 10:03:20 +0100

    Rumfanget

---
+++ Rumfanget/Rumfanget/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Skriv Længde");
+double length = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Skriv bredde");
+double width = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Skriv højde");
+double height = Double.Parse(Console.ReadLine());
+
+double result = length * height * width;
+
+Console.WriteLine("Rumfanget er: " + result);
+
