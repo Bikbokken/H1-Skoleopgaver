26a79b0b4d12cd0dcfa1a1ea005deb32c6c7f10d
Bikbokken <benjaminburek05@gmail.com>
2023-01-30 12:48:25 +0100

    Hold og lotto

---
+++ Held og lotto/Held og lotto/Program.cs
@@ -0,0 +1,38 @@
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+        do
+        {
+            Random random = new Random();
+            int randomNum = random.Next(1, 37);
+            if (!numbers.Contains(randomNum))
+            {
+                numbers.Add(randomNum);
+            }
+        } while(numbers.Count < 7); 
+       
+        numbers.Sort();
+        int joker = GetJoker(numbers);
+
+        foreach(int i in numbers)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            if (i == joker)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.Write(i);
+            Console.Write(" ");
+            Task.Delay(2000).Wait();
+        }
+    }
+    static int GetJoker(List<int> numbers)
+    {
+        Random random = new Random();
+        int randomNum = random.Next(0, numbers.Count());
+        return numbers.ElementAt(randomNum);
+    }
+}
