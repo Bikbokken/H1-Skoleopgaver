7dfb354708badae419f35db18f578ee0182c920b
Bikbokken <benjaminburek05@gmail.com>
2023-01-30 10:10:24 +0100

    Fødselsdagen

---
+++ Fødselsdagen/Fødselsdagen/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hvad er din fødselsdag dd/MM/yyyy");
+DateTime birthday = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+DateTime now = DateTime.Now;
+
+TimeSpan dif = now - birthday;
+int years = (int) Math.Floor(dif.TotalDays / 365.2425);
+
+int days = (int) Math.Floor((now - birthday.AddYears(years)).TotalDays);
+
+Console.WriteLine("Du er: " + years + " år og " + days + " dage gammel.");
+
+
+
