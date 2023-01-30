f03e867002c6dec0ec2a9ad72fb0aa3341b3b4b7
Bikbokken <bebu@dr.dk>
2023-01-30 09:19:46 +0100

    varaible og datatyper

---
+++ Variable og datatyper/Variable og datatyper/Program.cs
@@ -0,0 +1,18 @@
+﻿// OPG A
+
+int length = 16;
+double width = 5.1;
+
+//Console.WriteLine(length * width); // 81,6
+
+
+
+//OPG B
+//int area = width * length;
+//Det er ikke muligt at gange en int og en double og bruge datatypen "int area" - da dette ville give 81,6 (som skal være en double)
+
+
+
+//OPG C
+double d = width * length;
+// Dette er muligt, da disse to variabler ganget med hinanden giver en double
