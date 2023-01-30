e20c5459a5ae5a90a5215ebc53244469c1f16664
Bikbokken <benjaminburek05@gmail.com>
2023-01-30 10:00:16 +0100

    temperatur beregner

---
+++ Temperaturberegneren/Temperaturberegneren/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Skriv celius");
+double celcius = double.Parse(Console.ReadLine());
+
+double fahrenheit = (celcius * 1.8) + 32;
+double reamur = celcius * 0.8;
+
+Console.WriteLine("CELCIUS " + celcius);
+Console.WriteLine("REAMUR " + reamur);
+Console.WriteLine("FAHRENHEIT " + fahrenheit);
