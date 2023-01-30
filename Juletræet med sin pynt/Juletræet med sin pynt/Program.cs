fd03f4af4d6fe0345206228ef7d86207163b2eb8
Bikbokken <benjaminburek05@gmail.com>
2023-01-30 13:17:50 +0100

    Juletræet med sin pynt

---
+++ Juletræet med sin pynt/Juletræet med sin pynt/Program.cs
@@ -0,0 +1,25 @@
+﻿int height = 8;
+int dots = 1;
+int spaces = 15;
+
+for (int i = 0; i < height; i++)
+{
+    for (int x = 0; x < spaces; x++)
+    {
+        Console.Write(" ");
+    }
+    for (int y = 0; y < dots; y++)
+    {
+        if(i%2 != 0) { // er laget vi er på et odd number (altså hver anden)
+            if (y % 2 != 0) //er det nummer af dot vi er nået til et odd numer (igen hver anden)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+        }
+        Console.Write("* ");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    spaces = spaces-2;
+    dots = dots+2; 
+    Console.WriteLine(" ");
+}
