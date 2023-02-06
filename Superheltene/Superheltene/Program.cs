2d7cb905964b2ebf67f5a95456e8e13ded5ada3f
Bikbokken <benjaminburek05@gmail.com>
2023-02-06 09:16:38 +0100

    Superheletene

--- Superheltene/Superheltene/Program.cs
+++ Superheltene/Superheltene/Program.cs
@@ -1,9 +1,26 @@
-﻿public class SuperHero
+﻿public class Identity
 {
     private string _identity;
     private string _mask;
     private string _costume;
     private string _moral;
-    private List<string> _enemies;
-    private List<string> _abilities;
+    private List<Abilities> _abilities;
+
 }
+
+public class Villain
+{
+    private Identity _identity;
+    private List<SuperHero> _enemies;
+}
+public class SuperHero
+{
+    private Identity _identity;
+    private List<Villain> _enemies;
+}
+
+public class Abilities
+{
+    private string _name;
+}
+
