291054fec844710a459f182a3f0a739383243a0c
Bikbokken <benjaminburek05@gmail.com>
2023-02-06 08:58:26 +0100

    Josephus problemet

---
+++ Josephus problemet/Josephus problemet/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using Microsoft.VisualBasic;
+using System;
+using System.Net.Sockets;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int interval = 3;
+        int soilders = 41;
+        int index = 0;
+
+        interval--;
+        
+        List<int> soilderarray = new List<int>();
+
+
+        for (int i = 1; i <= soilders; i++)
+        {
+            soilderarray.Add(i);
+        }
+            
+
+        while(soilderarray.Count != 1)
+        {
+            index = (index + interval) % soilderarray.Count;
+            soilderarray.RemoveAt(index);
+
+        };
+        Console.WriteLine(soilderarray[0]);
+
+        
+    }
+   
+    
+}
