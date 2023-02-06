cbbc2a41700e3abbef0ef12c5104c78130a0d99b
Bikbokken <benjaminburek05@gmail.com>
2023-02-06 09:10:15 +0100

    Superheltene

---
+++ Superheltene/Superheltene/Program.cs
@@ -0,0 +1,9 @@
+﻿public class SuperHero
+{
+    private string _identity;
+    private string _mask;
+    private string _costume;
+    private string _moral;
+    private List<string> _enemies;
+    private List<string> _abilities;
+}
