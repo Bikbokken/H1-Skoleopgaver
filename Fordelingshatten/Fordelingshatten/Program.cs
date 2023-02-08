cdd735cf04a380b2aaaa5a73f1df7b1ceb92880b
Bikbokken <benjaminburek05@gmail.com>
2023-02-08 08:40:02 +0100

    Fordelingshatten

---
+++ Fordelingshatten/Fordelingshatten/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+
+public enum Colleges
+{
+    Gryffindor,
+    Hufflepuff,
+    Ravenclaw,
+    Slytherin,
+}
+
+class SorteringsHat
+{
+
+    private Colleges _college;
+    private Random random = new Random();
+
+    public Colleges College { get { return _college; } }    
+
+
+    public void GetCollege()
+    {
+        _college =(Colleges)random.Next(0, 4);
+    }
+}
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Du skal tilhøre:");
+        SorteringsHat sorteringsHat = new SorteringsHat();
+        sorteringsHat.GetCollege();
+        Console.WriteLine(sorteringsHat.College);
+
+    }
+
+
+}
